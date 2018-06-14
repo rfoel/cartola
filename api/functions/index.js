f286f5aa6d2608d5924b3e42ff9592dfad4400d6
Rafael Franco <rafaelfr@outlook.com>
2018-06-14 19:31:45 -0300

    Added login method

--- api/functions/index.js
+++ api/functions/index.js
@@ -1,13 +1,16 @@
 const functions = require('firebase-functions')
 const express = require('express')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 4000
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.listen(port, () => {
   console.log(`API listening on port ${port} in ${app.settings.env} mode`)
-  console.log(`http://localhost:${port}`)
   console.log(`http://localhost:${port}`)
 })
 
@@ -27,6 +30,30 @@ app.get('/partidas', (req, res) => {
 app.get('/times', (req, res) => {
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
   fetch(`https://api.cartolafc.globo.com/times?q=${req.query.q}`)
+    .then(res => res.json())
+    .then(json => {
+      res.send(json)
+    })
+})
+
+app.post('/entrar', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+
+  const { email, password } = req.body
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      payload: {
+        email,
+        password,
+        serviceId: 438
+      }
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  }
+
+  fetch('https://login.globo.com/api/authentication', options)
     .then(res => res.json())
     .then(json => {
       res.send(json)
